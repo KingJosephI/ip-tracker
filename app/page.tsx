26e91efdce64c0cff0f649bd725a0fcf58e7db0e
JosephKabemba <joseph.kabemba@academy.kinshasadigital.com>
2023-04-25 17:40:12 +0100

    feat: add search section content

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,34 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>IP Tracker App</h1>;
+  return (
+    <main>
+      <section className="search">
+        <h1>IP Address Tracker</h1>
+        <form>
+          <input placeholder="Search for any IP Adress or domain" />
+          <button>&gt;</button>
+        </form>
+        <div>
+          <div>
+            <h2>IP Adress</h2>
+            <p>192.212.174.101</p>
+          </div>
+          <div>
+            <h2>Location</h2>
+            <p>Brooklyn, NY 10001</p>
+          </div>
+          <div>
+            <h2>Timezone</h2>
+            <p>UTC -05:00</p>
+          </div>
+          <div>
+            <h2>ISP</h2>
+            <p>SpaceX Starlink</p>
+          </div>
+        </div>
+      </section>
+      <section className="map"></section>
+    </main>
+  );
 }
